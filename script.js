9ddce08dcccead5c72bebc9f48189668b45b64dc
awilix <217700573+awilix-23@users.noreply.github.com>
2025-07-23 23:39:57 -0700

    Add display dialog to create new book, need to add read status

--- script.js
+++ script.js
@@ -16,31 +16,49 @@ function addBook(title, author, year, isRead)
 }
 /******************************************************************************/
 /* Display */
-function Display()
+function Display(index)
 {
   const library = document.querySelector('#library');
 
-  for (i = 0; i <= myLibrary.length - 1; i++) {
-    const bookContainer = document.createElement('div');
-    bookContainer.className = "book";
+  const bookContainer = document.createElement('div');
+  bookContainer.className = "book";
 
-    const title = document.createElement('h2');
-    title.textContent = myLibrary[i].title;
-    bookContainer.appendChild(title);
-    const author = document.createElement('h3');
-    author.textContent = `By ${myLibrary[i].author}`;
-    bookContainer.appendChild(author);
-    const year = document.createElement('p');
-    year.textContent = `Year Published: ${myLibrary[i].year}`;
-    bookContainer.appendChild(year);
+  const title = document.createElement('h2');
+  title.textContent = myLibrary[index].title;
+  bookContainer.appendChild(title);
+  const author = document.createElement('h3');
+  author.textContent = `By ${myLibrary[index].author}`;
+  bookContainer.appendChild(author);
+  const year = document.createElement('p');
+  year.textContent = `Year Published: ${myLibrary[index].year}`;
+  bookContainer.appendChild(year);
 
-    library.appendChild(bookContainer);
-  }
+  library.appendChild(bookContainer);
 }
+
+const dialog = document.querySelector('.addBook');
+
+const showDialog = document.querySelector('#toolbar > button');
+const closeDialog = document.querySelector('dialog button');
+showDialog.addEventListener('click', () => dialog.showModal());
+closeDialog.addEventListener('click', () => dialog.close());
+
+const submitDialog = document.querySelector('.submit');
+const submitTitle = document.querySelector('#title');
+const submitAuthor = document.querySelector('#author');
+const submitYear = document.querySelector('#year');
+submitDialog.addEventListener('click', (event) => {
+  event.preventDefault();
+  addBook(submitTitle.value, submitAuthor.value, submitYear.value, false);
+  Display(myLibrary.length - 1);
+  dialog.close();
+});
 /******************************************************************************/
 /* Testing */
 addBook("The Fellowship of the Ring", "J. R. R. Tolkien", 1954, true);
 addBook("The Two Towers", "J. R. R. Tolkien", 1954, false);
 addBook("The Return of the King", "J. R. R. Tolkien", 1954, false);
 
-Display();
+for (i = 0; i <= myLibrary.length - 1; i++) {
+  Display(i); // Initialize
+}
